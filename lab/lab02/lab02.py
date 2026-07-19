17a667f73bdb52c62c744f45c1a075925c945927
YiHx <yhx2582413@gmail.com>
2026-07-19 16:58:01 +0800

    update

--- lab/lab02/lab02.py
+++ lab/lab02/lab02.py
@@ -74,43 +74,43 @@ def count_cond(condition):
     return counter
 
 
-def multiple(a, b):
-    """Return the smallest number n that is a multiple of both a and b.
+# def multiple(a, b):
+#     """Return the smallest number n that is a multiple of both a and b.
 
-    >>> multiple(3, 4)
-    12
-    >>> multiple(14, 21)
-    42
-    """
-    "*** YOUR CODE HERE ***"
+#     >>> multiple(3, 4)
+#     12
+#     >>> multiple(14, 21)
+#     42
+#     """
+#     "*** YOUR CODE HERE ***"
 
 
 
-def cycle(f1, f2, f3):
-    """Returns a function that is itself a higher-order function.
+# def cycle(f1, f2, f3):
+#     """Returns a function that is itself a higher-order function.
 
-    >>> def add1(x):
-    ...     return x + 1
-    >>> def times2(x):
-    ...     return x * 2
-    >>> def add3(x):
-    ...     return x + 3
-    >>> my_cycle = cycle(add1, times2, add3)
-    >>> identity = my_cycle(0)
-    >>> identity(5)
-    5
-    >>> add_one_then_double = my_cycle(2)
-    >>> add_one_then_double(1)
-    4
-    >>> do_all_functions = my_cycle(3)
-    >>> do_all_functions(2)
-    9
-    >>> do_more_than_a_cycle = my_cycle(4)
-    >>> do_more_than_a_cycle(2)
-    10
-    >>> do_two_cycles = my_cycle(6)
-    >>> do_two_cycles(1)
-    19
-    """
-    "*** YOUR CODE HERE ***"
+#     >>> def add1(x):
+#     ...     return x + 1
+#     >>> def times2(x):
+#     ...     return x * 2
+#     >>> def add3(x):
+#     ...     return x + 3
+#     >>> my_cycle = cycle(add1, times2, add3)
+#     >>> identity = my_cycle(0)
+#     >>> identity(5)
+#     5
+#     >>> add_one_then_double = my_cycle(2)
+#     >>> add_one_then_double(1)
+#     4
+#     >>> do_all_functions = my_cycle(3)
+#     >>> do_all_functions(2)
+#     9
+#     >>> do_more_than_a_cycle = my_cycle(4)
+#     >>> do_more_than_a_cycle(2)
+#     10
+#     >>> do_two_cycles = my_cycle(6)
+#     >>> do_two_cycles(1)
+#     19
+#     """
+#     "*** YOUR CODE HERE ***"
 
