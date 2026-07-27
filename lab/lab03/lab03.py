8f3856caba320e9b493eac7a36adba5c54000fb7
YiHx <yhx2582413@gmail.com>
2026-07-27 17:15:02 +0800

    update

--- lab/lab03/lab03.py
+++ lab/lab03/lab03.py
@@ -70,7 +70,7 @@ def squares(s):
     >>> squares(seq)
     []
     """
-    return [___ for n in s if ___]
+    return [int(sqrt(n)) for n in s if sqrt(n)==round(sqrt(n))]
 
 
 def double_eights(n):
